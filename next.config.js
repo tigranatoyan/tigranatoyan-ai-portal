758a7e2a91b6ae247d0147b6b37435f92d491252
Tigran Atoyan <tigran.atoyan80@gmail.com>
2026-05-02 15:21:43 +0400

    Add static export config and GitHub Pages deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  poweredByHeader: false
+  poweredByHeader: false,
+  output: 'export',
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
